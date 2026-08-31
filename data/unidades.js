23be30bbd6f92196c65059cc5581f7bf74164410
lucasvenancio0110 <lucassantanals0110@gmail.com>
2026-08-31 13:42:05 -0300

    feat: add UnimedLab unit directory

---
+++ data/unidades.js
@@ -0,0 +1,29 @@
+export const UNIDADES = [
+  { id: 'mega', name: 'Megaunidade', address: 'Avenida Iguaçu, 1815', neighborhood: 'Água Verde', city: 'Curitiba', cep: '80240-030', type: 'mega' },
+  { id: 'ahu', name: 'Unidade Ahú', address: 'Avenida Anita Garibaldi, 2893', neighborhood: 'Ahú', city: 'Curitiba', cep: '82210-000' },
+  { id: 'bacacheri', name: 'Unidade Bacacheri', address: 'Rua Nicarágua, 847', neighborhood: 'Bacacheri', city: 'Curitiba', cep: '82510-170' },
+  { id: 'bairro-alto', name: 'Unidade Bairro Alto', address: 'Rua Alberico Flores Bueno, 438', neighborhood: 'Bairro Alto', city: 'Curitiba', cep: '82840-170' },
+  { id: 'cajuru', name: 'Unidade Cajuru', address: 'Rua Vicente de Carvalho, 113', neighborhood: 'Cajuru', city: 'Curitiba', cep: '82940-370' },
+  { id: 'centro', name: 'Unidade Centro', address: 'Rua Emiliano Perneta, 466', neighborhood: 'Centro', city: 'Curitiba', cep: '80420-080' },
+  { id: 'fazendinha', name: 'Unidade Fazendinha', address: 'Rua Carlos Klemtz, 1553', neighborhood: 'Fazendinha', city: 'Curitiba', cep: '81320-000' },
+  { id: 'germano-mayer', name: 'Unidade Germano Mayer', address: 'Rua Padre Germano Mayer, 840', neighborhood: 'Alto da XV', city: 'Curitiba', cep: '80040-170' },
+  { id: 'itupava', name: 'Unidade Itupava', address: 'Rua Itupava, 737', neighborhood: 'Alto da XV', city: 'Curitiba', cep: '80040-134' },
+  { id: 'palladium', name: 'Unidade Palladium', address: 'Avenida Presidente Kennedy, 4121', detail: 'Shopping Palladium', neighborhood: 'Portão', city: 'Curitiba', cep: '80610-905', type: 'shopping' },
+  { id: 'portao', name: 'Unidade Portão', address: 'Avenida Presidente Arthur da Silva Bernardes, 2285, Loja 02', neighborhood: 'Portão', city: 'Curitiba', cep: '80320-300' },
+  { id: 'santa-felicidade', name: 'Unidade Santa Felicidade', address: 'Rua Domingos Strapasson, 100, Lojas 3 e 4', neighborhood: 'Santa Felicidade', city: 'Curitiba', cep: '82320-040' },
+  { id: 'sitio-cercado', name: 'Unidade Sítio Cercado', address: 'Rua Nova Aurora, 1770, Lojas 04 e 05', neighborhood: 'Sítio Cercado', city: 'Curitiba', cep: '81920-650' },
+  { id: 'xaxim', name: 'Unidade Xaxim', address: 'Rua Francisco Derosso, 3488, Lojas 55, 56 e 57', neighborhood: 'Xaxim', city: 'Curitiba', cep: '81830-285' },
+  { id: 'hospital-fatima', name: 'Hospital e Maternidade Nossa Senhora de Fátima', address: 'Avenida Visconde de Guarapuava, 3077', neighborhood: 'Centro', city: 'Curitiba', cep: '80010-100', type: 'hospital' },
+  { id: 'parkshopping-barigui', name: 'ParkShoppingBarigüi', address: 'Rua Professor Pedro Viriato Parigot de Souza, 600', detail: 'Centro Médico, Piso L3', neighborhood: 'Mossunguê', city: 'Curitiba', cep: '81200-100', type: 'shopping' },
+  { id: 'shopping-mueller', name: 'Shopping Mueller', address: 'Avenida Cândido de Abreu, 127', detail: 'Piso L1', neighborhood: 'Centro Cívico', city: 'Curitiba', cep: '80530-900', type: 'shopping' },
+  { id: 'emiliano-perneta', name: 'Unidade Emiliano Perneta', address: 'Rua Emiliano Perneta, 756', neighborhood: 'Centro', city: 'Curitiba', cep: '80420-080', type: 'new' },
+  { id: 'pinhais', name: 'Unidade Pinhais', address: 'Avenida Jacob Macanhan, 736', neighborhood: 'Centro', city: 'Pinhais', cep: '83324-420' },
+  { id: 'campo-largo', name: 'Unidade Campo Largo', address: 'Rua Marechal Deodoro, 477, Loja 2', neighborhood: 'Centro', city: 'Campo Largo', cep: '83601-020' },
+  { id: 'fazenda-rio-grande', name: 'Unidade Fazenda Rio Grande', address: 'Rua César Carelli, 122', neighborhood: 'Centro', city: 'Fazenda Rio Grande', cep: '83833-054' },
+  { id: 'araucaria-centro', name: 'Araucária – Centro', address: 'Avenida Doutor Victor do Amaral, 523, Loja 2', neighborhood: 'Centro', city: 'Araucária', cep: '83702-040' },
+  { id: 'araucaria-iguacu', name: 'Araucária – Iguaçu', address: 'Rua Miguel Bertolino Pizatto, 1901', detail: 'Centro Médico Araucária', neighborhood: 'Iguaçu', city: 'Araucária', cep: '83701-050' },
+  { id: 'afonso-pena', name: 'Unidade Afonso Pena', address: 'Avenida Rui Barbosa, 4910, Loja 1', neighborhood: 'Afonso Pena', city: 'São José dos Pinhais', cep: '83045-350' },
+  { id: 'shopping-sao-jose', name: 'Shopping São José', address: 'Rua Izabel A Redentora, 1434', detail: 'Shopping São José', neighborhood: 'Centro', city: 'São José dos Pinhais', cep: '83005-010', type: 'shopping' }
+];
+
+export const CITIES = ['Todas', 'Curitiba', 'Araucária', 'São José dos Pinhais', 'Pinhais', 'Campo Largo', 'Fazenda Rio Grande'];
